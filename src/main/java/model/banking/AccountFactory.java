60d37474a242eb91db212e73b2b52b1de8ea41b0
robert miller <robert.miller@fau.de>
2017-05-16 15:46:33 +0200

    story 7: account factory for creating new accounts at the api endpoint

---
+++ src/main/java/model/banking/AccountFactory.java
@@ -0,0 +1,34 @@
+package model.banking;
+
+import api.BankingRESTClient;
+import com.amazonaws.util.json.JSONException;
+import com.amazonaws.util.json.JSONObject;
+import model.banking.account.Account;
+
+/**
+ * Factory to create new accounts at the endpoint
+ */
+public class AccountFactory {
+
+    /**
+     *
+     */
+    private static AccountFactory accountFactory = new AccountFactory();
+
+
+    public static AccountFactory getInstance() {
+        return accountFactory;
+    }
+
+    public Account createAccount(String number, Double balance, String openingDate) throws JSONException {
+
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("number", number);
+        jsonObject.put("balance", balance);
+        jsonObject.put("openingDate", openingDate);
+
+        BankingRESTClient bankingRESTClient = BankingRESTClient.getInstance();
+        return (Account) bankingRESTClient.postBankingModelObject("/api/v1_0/accounts/generate", jsonObject.toString(), Account.class);
+    }
+}
