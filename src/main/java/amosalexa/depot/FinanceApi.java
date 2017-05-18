c2b4dfc50bd5e2c64e2cc3f5b257681314ebfae0
PaulAhuevo <nieblerpaul@gmail.com>
2017-05-18 21:42:39 +0200

    Depotabfragen hinzugefügt.

---
+++ src/main/java/amosalexa/depot/FinanceApi.java
@@ -0,0 +1,58 @@
+package amosalexa.depot;
+
+
+import com.amazonaws.util.json.JSONException;
+import com.amazonaws.util.json.JSONObject;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import java.io.IOException;
+
+public class FinanceApi {
+    OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public static String getStockPrice(String stock) {
+
+        try {
+            FinanceApi finance = new FinanceApi();
+            String response = finance.run("http://finance.google.com/finance/info?client=ig&q=NASDAQ:" + stock);
+
+            response = response.substring(5, response.length() - 2);
+
+            //create a JSON-Object of the String
+            final JSONObject obj = new JSONObject(response);
+
+            // read out the current stock price
+            //Gson gson = new Gson();
+
+
+            JsonObject jsonObject = new JsonParser().parse(response).getAsJsonObject();
+
+            // JsonObject jsonObject = new JsonParser().parse("{\"pcls_fix\": \"68.38\"}").getAsJsonObject();
+            String stockPrice = jsonObject.get("pcls_fix").getAsString();
+
+            return stockPrice;
+        }
+        catch (IOException io) {
+            System.out.println("Error: " + io);
+        }
+        catch (JSONException json) {
+            System.out.println("Error: " + json);
+        }
+
+        return null;
+
+    }
+}
