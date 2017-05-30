20de0c0babdbf0904f36f9527edc7eab6327608d
Julian Matschinske <julian.matschinske@fau.de>
2017-05-30 15:56:29 +0200

    Add more tests and helper methods that make testing more convenient

--- src/test/java/amosalexa/AmosAlexaSpeechletTest.java
+++ src/test/java/amosalexa/AmosAlexaSpeechletTest.java
@@ -17,6 +17,8 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
@@ -26,30 +28,91 @@ public class AmosAlexaSpeechletTest {
     private Session session;
     private final String SESSION_ID = "SessionId.2682fed6-193f-48b3-afd7-c6185d075ddf";
 
+    /*************************************
+     *          Testing section          *
+     *************************************/
+
     @Test
     public void blockCardIntentTest() throws Exception {
         newSession();
+
         testIntent(
                 "BlockCardIntent", "BankCardNumber:123",
                 "Möchten Sie die Karte 123 wirklich sperren?");
+
         testIntent(
                 "AMAZON.YesIntent",
                 "Karte 123 wurde gesperrt.");
     }
 
     @Test
-    public void SavingsPlanTest() throws Exception {
+    public void savingsPlanTest() throws Exception {
         newSession();
-        testIntent(
+
+        testIntentMatches(
                 "SavingsPlanIntent", "AnzahlJahre:2", "EinzahlungMonat:150", "Grundbetrag:1500",
-                "<speak>Bei einem Zinssatz von zwei Prozent waere der Gesamtsparbetrag am Ende des Zeitraums insgesamt <say-as interpret-as=\"number\">5231,01</say-as> Euro. Soll ich diesen Sparplan fuer dich anlegen?</speak>");
+                "Bei einem Zinssatz von zwei Prozent waere der Gesamtsparbetrag am Ende des Zeitraums insgesamt (.*) Euro\\. Soll ich diesen Sparplan fuer dich anlegen\\?"
+        );
 
         Calendar calendar = Calendar.getInstance();
         String nextPayin = String.format("01.%02d.%d", calendar.get(Calendar.MONTH) + 2, calendar.get(Calendar.YEAR));
 
         testIntent(
                 "AMAZON.YesIntent",
-                "Okay! Ich habe den Sparplan angelegt. Der Grundbetrag von 1500 Euro wird deinem Sparkonto gutgeschrieben. Die erste regelmaeßige Einzahlung von 150 Euro erfolgt am "+nextPayin+".");
+                "Okay! Ich habe den Sparplan angelegt. Der Grundbetrag von 1500 Euro wird deinem Sparkonto gutgeschrieben. Die erste regelmaeßige Einzahlung von 150 Euro erfolgt am " + nextPayin + ".");
+    }
+
+    @Test
+    public void replacementCardDialogTest() throws Exception {
+        newSession();
+
+        String response = testIntentMatches("ReplacementCardIntent",
+                "Bestellung einer Ersatzkarte. Es wurden folgende Karten gefunden: (.*)");
+
+        Pattern p = Pattern.compile("Kreditkarte mit den Endziffern ([0-9]+)\\.");
+        Matcher m = p.matcher(response);
+
+        if (m.find()) {
+            String endDigits = m.group(1);
+            testIntent("FourDigitNumberIntent", "FourDigits:" + endDigits,
+                    "Wurde die Karte gesperrt oder wurde sie beschädigt?");
+
+            testIntent("ReplacementCardReasonIntent", "ReplacementReason:beschaedigt",
+                    "Soll ein Ersatz für die beschädigte Karte mit den Endziffern " + endDigits + " bestellt werden?");
+
+            testIntent(
+                    "AMAZON.YesIntent",
+                    "Okay, eine Ersatzkarte wurde bestellt.");
+        } else {
+            assertTrue(false);
+        }
+    }
+
+    /************************************
+     *          Helper methods          *
+     ************************************/
+
+    private String testIntentMatches(String intent, String... params) throws IOException, NoSuchFieldException, IllegalAccessException {
+        String[] slots = new String[params.length - 1];
+        String expectedOutput = null;
+
+        int i = 0;
+        for (String param : params) {
+            if (i == params.length - 1) {
+                expectedOutput = param;
+            } else {
+                slots[i] = param;
+                i++;
+            }
+        }
+
+        String actual = performIntent(intent, slots);
+
+        boolean condition = actual.matches(expectedOutput);
+
+        assertTrue("[MATCHING]\nActual: " + actual + "\nExpected: " + expectedOutput, condition);
+
+        return actual;
     }
 
     private void testIntent(String intent, String... params) throws IOException, NoSuchFieldException, IllegalAccessException {
@@ -68,16 +131,31 @@ public class AmosAlexaSpeechletTest {
 
         AmosAlexaSpeechlet amosAlexaSpeechlet = AmosAlexaSpeechlet.getInstance();
         SpeechletResponse response = amosAlexaSpeechlet.onIntent(getEnvelope(intent, slots));
-        assertEquals(expectedOutput, getOutputSpeechText(response.getOutputSpeech()));
+        assertEquals(expectedOutput, performIntent(intent, slots));
+    }
+
+    private String performIntent(String intent, String... params) throws IOException, NoSuchFieldException, IllegalAccessException {
+        String[] slots = new String[params.length];
+
+        int i = 0;
+        for (String param : params) {
+            slots[i] = param;
+            i++;
+        }
+
+        AmosAlexaSpeechlet amosAlexaSpeechlet = AmosAlexaSpeechlet.getInstance();
+        SpeechletResponse response = amosAlexaSpeechlet.onIntent(getEnvelope(intent, slots));
+        return getOutputSpeechText(response.getOutputSpeech())
+                .replaceAll("\\<.*?>", ""); // Remove all markup since it is not really relevant for our tests
     }
 
     private String getOutputSpeechText(OutputSpeech outputSpeech) {
         if (outputSpeech instanceof SsmlOutputSpeech) {
-            SsmlOutputSpeech ssmlOutputSpeech = (SsmlOutputSpeech)outputSpeech;
+            SsmlOutputSpeech ssmlOutputSpeech = (SsmlOutputSpeech) outputSpeech;
             return ssmlOutputSpeech.getSsml();
         }
         if (outputSpeech instanceof PlainTextOutputSpeech) {
-            PlainTextOutputSpeech plainTextOutputSpeech = (PlainTextOutputSpeech)outputSpeech;
+            PlainTextOutputSpeech plainTextOutputSpeech = (PlainTextOutputSpeech) outputSpeech;
             return plainTextOutputSpeech.getText();
         }
 
