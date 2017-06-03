a19027e73608904906a206f1a1da9360968ab50e
Julian Matschinske <julian.matschinske@fau.de>
2017-06-03 18:26:57 +0200

    Make method static to reuse it in other classes

--- src/main/java/amosalexa/AmosAlexaSpeechlet.java
+++ src/main/java/amosalexa/AmosAlexaSpeechlet.java
@@ -342,4 +342,28 @@ public class AmosAlexaSpeechlet implements SpeechletSubject {
         return SpeechletResponse.newAskResponse(speech, reprompt, card);
     }
 
+    public static SpeechletResponse getSpeechletResponse(String speechText, String repromptText,
+                                                         boolean isAskResponse) {
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("Block Bank Card");
+        card.setContent(speechText);
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        if (isAskResponse) {
+            // Create reprompt
+            PlainTextOutputSpeech repromptSpeech = new PlainTextOutputSpeech();
+            repromptSpeech.setText(repromptText);
+            Reprompt reprompt = new Reprompt();
+            reprompt.setOutputSpeech(repromptSpeech);
+
+            return SpeechletResponse.newAskResponse(speech, reprompt, card);
+        } else {
+            return SpeechletResponse.newTellResponse(speech, card);
+        }
+    }
+
 }
